19489ed947552164609dec86e10902176cdf04a2
Theo <richar_o@epita.fr>
2011-03-07 07:24:31 -0500

    partials, haml, ...

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Ror::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/contact"
+
+  resources :microposts
+
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
